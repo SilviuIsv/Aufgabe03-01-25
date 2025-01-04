25e6594f79a2550b8c7bb12958bfcf4aaa57e999
SilviuIsv <79253267+SilviuIsv@users.noreply.github.com>
2025-01-04 10:51:19 +0100

    Updated Hero.java

--- src/main/java/org/example/Hero.java
+++ src/main/java/org/example/Hero.java
@@ -1,15 +1,1 @@
-package org.example;
-
-public class Hero {
-    private String name;
-    private int health;
-    private int attackPower;
-    private String items;
-
-    public Hero(String name, int health, int attackPower, String items) {
-        this.name = name;
-        this.health = health;
-        this.attackPower = attackPower;
-        this.items = items;
-    }
-}
+content modificat in base64
