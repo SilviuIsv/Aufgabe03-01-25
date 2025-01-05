e136b389c18003990d42f4e989737d0c252bbdfd
SilviuIsv <79253267+SilviuIsv@users.noreply.github.com>
2025-01-05 12:34:56 +0100

    Updated file

--- src/main/java/org/example/Monster.java
+++ src/main/java/org/example/Monster.java
@@ -8,14 +8,26 @@ public class Monster {
 
     private void attack(Hero hero){
 
+        public void roar() {
+        System.out.println(name + " roars menacingly!");
     }
+}
     private void takeDamage(int damage){
 
+        public void roar() {
+        System.out.println(name + " roars menacingly!");
     }
+}
 
     public Monster(String name, int health, int takeDamage) {
         this.name = name;
         this.health = health;
         this.attckPower = attckPower;
+        public void roar() {
+        System.out.println(name + " roars menacingly!");
+    }
+}
+    public void roar() {
+        System.out.println(name + " roars menacingly!");
     }
 }
