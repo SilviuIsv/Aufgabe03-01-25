8e4d4084c3f5f5544a497f4b9676e6624a8c707b
SilviuIsv <79253267+SilviuIsv@users.noreply.github.com>
2025-01-05 12:42:18 +0100

    Updated file

--- src/main/java/org/example/Hero.java
+++ src/main/java/org/example/Hero.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class Hero {
+    private int innerDemons;
     private String name;
     private int health;
     private int attackPower;
@@ -12,5 +13,33 @@ public Hero(String name, int health, int attackPower, String items) {
         this.health = health;
         this.attackPower = attackPower;
         this.items = items;
+        public void meditate(int time) {
+        innerDemons -= time / 10;
+        if (innerDemons < 0) innerDemons = 0;
+        System.out.println(name + " meditates for " + time + " minutes, fighting inner demons.");
+    }
+
+    public void fightInnerDemons() {
+        if (innerDemons > 0) {
+            System.out.println(name + " fights inner demons fiercely.");
+            innerDemons -= 1;
+        } else {
+            System.out.println(name + " is at peace. No inner demons left.");
+        }
+    }
+}
+    public void meditate(int time) {
+        innerDemons -= time / 10;
+        if (innerDemons < 0) innerDemons = 0;
+        System.out.println(name + " meditates for " + time + " minutes, fighting inner demons.");
+    }
+
+    public void fightInnerDemons() {
+        if (innerDemons > 0) {
+            System.out.println(name + " fights inner demons fiercely.");
+            innerDemons -= 1;
+        } else {
+            System.out.println(name + " is at peace. No inner demons left.");
+        }
     }
 }
